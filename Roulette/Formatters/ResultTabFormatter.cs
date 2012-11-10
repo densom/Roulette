fe5cd4339754384d951c30116fadaf126d8e52cf
Dennis Somerville <dennis.somerville@gmail.com>
2012-11-10 09:22:48 -0500

    Refactored tab formatter.

--- Roulette/Formatters/ResultTabFormatter.cs
+++ Roulette/Formatters/ResultTabFormatter.cs
@@ -4,15 +4,21 @@ namespace Roulette.Formatters
 {
     internal class ResultTabFormatter : ResultFormatterBase
     {
+        private string delimiter = "\t";
+
         public override string GetHeader()
         {
-            return string.Join("\t", HeaderTextList);
+            return string.Join(delimiter, HeaderTextList);
         }
 
         public override string GetDetail(ResultDataItem item)
         {
-            //todo: remove the tab formatting from Bet/Result
-            return string.Join("\t", item.Bankroll, item.Bet, item.Result);
+            return string.Join(delimiter, 
+                string.Format("{0:c}",item.Bankroll), 
+                item.Bet.Color, 
+                string.Format("{0:c}",item.Bet.Amount), 
+                string.Format("{0:c}",item.Result.BetAmount), 
+                item.Result.IsWin);
         }
     }
 }
