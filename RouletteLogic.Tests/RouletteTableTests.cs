bcbbf3c0f43b31146beec7712e1993d9d155e59b
Dennis Somerville <dennis.somerville@gmail.com>
2012-11-11 06:49:35 -0500

    untracked files on master: ed25458 refactored.

---
+++ RouletteLogic.Tests/RouletteTableTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace RouletteLogic.Tests
+{
+    [TestFixture]
+    public class RouletteTableTests
+    {
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Table_EnsureLimitGreaterThanZero()
+        {
+            var table = new RouletteTable(-1);
+
+        }
+
+        [Test]
+        public void notsureyet()
+        {
+            var table = new RouletteTable(100);
+        }
+
+
+    }
+}
